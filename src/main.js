7468961aee31c925a8599669f9a0109eedc9e797
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-21 11:37:28 +0530

    cors.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const user = require("./user");
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/user", (req, res) => {
   const list = user.getAllUser();
